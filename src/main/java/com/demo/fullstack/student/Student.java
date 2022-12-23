8bb04b5d4bc06c87a1888e41ea992dfda11e3c2d
1Dimitrios3 <dimitrismavrokefalos@gmail.com>
2022-12-23 19:03:23 +0200

    Initialize_basic_server_for_students_crud

---
+++ src/main/java/com/demo/fullstack/student/Student.java
@@ -0,0 +1,48 @@
+package com.demo.fullstack.student;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
